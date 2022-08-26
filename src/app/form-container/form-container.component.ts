76edf6eb5aaa1e22ddc04d408b1c0fa5294ba863
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-08-26 11:15:35 -0300

    ✨ feat: create form orchestrator

--- src/app/form-container/form-container.component.ts
+++ src/app/form-container/form-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'msf-form-container',
@@ -11,5 +11,25 @@ export class FormContainerComponent implements OnInit {
 
   form: FormGroup;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      step1: new FormGroup({
+        firstName: new FormControl(null, Validators.required),
+        lastName: new FormControl(null, Validators.required),
+        bithDate: new FormControl(null, Validators.required),
+      }),
+      step2: new FormGroup({
+        address: new FormControl(null),
+        country: new FormControl(null),
+      }),
+    });
+  }
+
+  getStep1Form(): FormGroup {
+    return this.form.get('step1') as FormGroup;
+  }
+
+  getStep2Form(): FormGroup {
+    return this.form.get('step2') as FormGroup;
+  }
 }
