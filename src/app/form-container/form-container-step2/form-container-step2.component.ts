b71f3c5119f8bbf5bdb1dd6c4ea43be61201dc6d
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-08-26 12:04:53 -0300

    ✨ feat: creation of step2 component rules and screen

--- src/app/form-container/form-container-step2/form-container-step2.component.ts
+++ src/app/form-container/form-container-step2/form-container-step2.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormContainerComponent } from './../form-container.component';
 
 @Component({
   selector: 'msf-form-container-step2',
   templateUrl: './form-container-step2.component.html',
-  styleUrls: ['./form-container-step2.component.scss']
+  styleUrls: ['./form-container-step2.component.scss'],
 })
 export class FormContainerStep2Component implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formContainer: FormContainerComponent, private readonly router: Router) {}
 
   ngOnInit(): void {
+    this.form = this.formContainer.getStep2Form();
   }
 
+  submit(): void {
+    alert('Nice Work!!✅');
+    this.formContainer.clearForm();
+    this.router.navigate(['']);
+  }
 }
--- src/app/form-container/form-container-step2/form-container-step2.module.ts
+++ src/app/form-container/form-container-step2/form-container-step2.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { FormContainerStep2RoutingModule } from './form-container-step2-routing.module';
 import { FormContainerStep2Component } from './form-container-step2.component';
 
 @NgModule({
   declarations: [FormContainerStep2Component],
-  imports: [CommonModule, FormContainerStep2RoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, FormContainerStep2RoutingModule, ReactiveFormsModule, MatButtonModule, MatInputModule],
 })
 export class FormContainerStep2Module {}
