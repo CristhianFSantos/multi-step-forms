d3acaaf5440b98b56ac9e04cef57fcd782c655fb
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-08-26 12:17:19 -0300

    ✨ feat: create directive local storage save data form

--- src/app/form-container/form-container.module.ts
+++ src/app/form-container/form-container.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormStorageModule } from './../form-storage/form-storage.module';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormContainerRoutingModule } from './form-container-routing.module';
@@ -7,6 +8,6 @@ import { FormContainerComponent } from './form-container.component';
 
 @NgModule({
   declarations: [FormContainerComponent],
-  imports: [CommonModule, FormContainerRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, FormContainerRoutingModule, ReactiveFormsModule, FormStorageModule],
 })
 export class FormContainerModule {}
---
+++ src/app/form-storage/form-storage.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormStorageDirective } from './form-storage.directive';
+
+@NgModule({
+  declarations: [FormStorageDirective],
+  imports: [CommonModule],
+})
+export class FormStorageModule {}
